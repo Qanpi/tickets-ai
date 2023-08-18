517861f22577e0275c287d0b82c3b2b2e0b142c4
Qanpi <aleksei.terin@outlook.com>
2023-08-19 01:21:58 +0300

    tickets with relative x&y coords

---
+++ samples/Save Ticket PNG.jsx
@@ -0,0 +1,21 @@
+﻿preferences.rulerUnits = Units.PIXELS;
+
+const doc = app.activeDocument;
+const layer = doc.activeLayer;
+
+const sampler = doc.colorSamplers[0];
+
+var x = sampler.position[0];
+var y = sampler.position[1];
+
+x /= doc.width;
+y /= doc.height;
+
+const outPath = "C:/Users/aleks/OneDrive - Suomalaisen Yhteiskoulun Osakeyhtiö/Tiedostot/Coding/EE/tickets-ai/samples/";
+const filename =  outPath + layer.name + ", x=" + x.toFixed(3) + ", y=" + y.toFixed(3) + ".png";
+const pngFile = new File(filename);
+const pngOptions = new PNGSaveOptions();
+pngOptions.compression = 5;
+
+doc.saveAs(pngFile, pngOptions);
+
