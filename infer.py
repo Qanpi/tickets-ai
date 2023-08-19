f9a06ab1198c82ac833c1f7f914e720cb47d7005
Qanpi <aleksei.terin@outlook.com>
2023-08-19 13:13:24 +0300

    included option to validate

--- infer.py
+++ infer.py
@@ -30,54 +30,72 @@ from models import UNet, FCRN_A
 
 
 @click.command()
-@click.option('-i', '--infer_path',
-              type=click.File('r'),
-              required=True,
-              help="A path to an input image to infer.")
-@click.option('-n', '--network_architecture',
-              type=click.Choice(['UNet', 'FCRN_A']),
-              required=True,
-              help='Model architecture.')
-@click.option('-c', '--checkpoint',
-              type=click.File('r'),
-              required=True,
-              help='A path to a checkpoint with weights.')
-@click.option('--unet_filters', default=64,
-              help='Number of filters for U-Net convolutional layers.')
-@click.option('--convolutions', default=2,
-              help='Number of layers in a convolutional block.')
-@click.option('--one_channel',
-              is_flag=True,
-              help="Turn this on for one channel images (required for ucsd).")
-@click.option('--pad',
-              is_flag=True,
-              help="Turn on padding for input image (required for ucsd).")
-@click.option('--visualize',
-              is_flag=True,
-              help="Visualize predicted density map.")
-
-def infer(infer_path: str,
-          network_architecture: str,
-          checkpoint: str,
-          unet_filters: int,
-          convolutions: int,
-          one_channel: bool,
-          pad: bool,
-          visualize: bool):
+@click.option(
+    "-i",
+    "--infer_path",
+    type=click.File("r"),
+    required=True,
+    help="A path to an input image to infer.",
+)
+@click.option(
+    "-n",
+    "--network_architecture",
+    type=click.Choice(["UNet", "FCRN_A"]),
+    required=True,
+    help="Model architecture.",
+)
+@click.option(
+    "-c",
+    "--checkpoint",
+    type=click.File("r"),
+    required=True,
+    help="A path to a checkpoint with weights.",
+)
+@click.option(
+    "--unet_filters",
+    default=64,
+    help="Number of filters for U-Net convolutional layers.",
+)
+@click.option(
+    "--convolutions", default=2, help="Number of layers in a convolutional block."
+)
+@click.option(
+    "--one_channel",
+    is_flag=True,
+    help="Turn this on for one channel images (required for ucsd).",
+)
+@click.option(
+    "--pad", is_flag=True, help="Turn on padding for input image (required for ucsd)."
+)
+@click.option(
+    "-v",
+    "--valid_path",
+    type=click.File("r"),
+    help="A path to an answer image containing true keypoints.",
+)
+@click.option("--visualize", is_flag=True, help="Visualize predicted density map.")
+def infer(
+    infer_path: str,
+    valid_path: str,
+    network_architecture: str,
+    checkpoint: str,
+    unet_filters: int,
+    convolutions: int,
+    one_channel: bool,
+    pad: bool,
+    visualize: bool,
+):
     """Run inference for a single image."""
     # use GPU if available
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # only UCSD dataset provides greyscale images instead of RGB
     input_channels = 1 if one_channel else 3
 
     # initialize a model based on chosen network_architecture
-    network = {
-        'UNet': UNet,
-        'FCRN_A': FCRN_A
-    }[network_architecture](input_filters=input_channels,
-                            filters=unet_filters,
-                            N=convolutions).to(device)
+    network = {"UNet": UNet, "FCRN_A": FCRN_A}[network_architecture](
+        input_filters=input_channels, filters=unet_filters, N=convolutions
+    ).to(device)
 
     # load provided state dictionary
     # note: by default train.py saves the model in data parallel mode
@@ -89,7 +107,7 @@ def infer(infer_path: str,
 
     # padding was applied for ucsd images to allow down and upsampling
     if pad:
-        img = Image.fromarray(np.pad(img, 1, 'constant', constant_values=0))
+        img = Image.fromarray(np.pad(img, 1, "constant", constant_values=0))
 
     # network's output represents a density map
     density_map = network(TF.to_tensor(img).unsqueeze_(0))
@@ -99,43 +117,49 @@ def infer(infer_path: str,
 
     print(f"The number of objects found: {n_objects}")
 
-    if visualize:
-        _visualize(img, density_map.squeeze().cpu().detach().numpy())
+    keypoints = None
 
-def _visualize(img, dmap):
+    if valid_path is not None:
+        answer_key = np.array(Image.open(valid_path.name).convert("1"))
+        keypoints = np.argwhere(answer_key == 1)
+
+        print(f"The true number of objects: {keypoints.shape[0]}")
+
+    if visualize:
+        _visualize(img, density_map.squeeze().cpu().detach().numpy(), keypoints)
+
+def _visualize(img, dmap, keypoints):
     """Draw a density map onto the image."""
     # keep the same aspect ratio as an input image
     fig, axes = plt.subplots(1, 2)
 
-    #turn off axis ticks
+    # turn off axis ticks
     [ax.axis("off") for ax in axes]
 
-    #normalize dmap between 0 and 255 
-    kernel = np.full((4, 4), 1)
-    peaks = peak_local_max(dmap, footprint=kernel, min_distance=2, num_peaks=28, exclude_border=False)
-
-    marks_x = [xy[1] for xy in peaks]
-    marks_y = [xy[0] for xy in peaks]
-
-    # mask = (dmap - np.min(dmap))*(300 / np.max(dmap))
-    # mask = mask.astype(np.uint8)
-
-    # add a alpha channel proportional to a density map value
-    # red = Image.new("RGB", img.size, (0, 255, 0))
-    # red.putalpha(Image.fromarray(mask, mode="L"))
-
-    # display an image with density map put on top of it
-    # overlaid = Image.alpha_composite(img.convert('RGBA'), red)
-
-    #display raw density map
+    # display raw density map
     axes[0].imshow(dmap, cmap="hot")
-    # axes[0].scatter(x=marks_x, y =marks_y, c=(0, 1, 0), s=10) 
 
-    # plot a density map without axis, and density map over og image
+    # overlay true keypoints
+    if keypoints is not None:
+        true_x = [xy[1] for xy in keypoints]
+        true_y = [xy[0] for xy in keypoints]
+
+        axes[0].scatter(x=true_x, y=true_y, c="none", s=50, marker="s", edgecolors="#0000ff")
+        axes[1].scatter(x=true_x, y=true_y, c="none", s=50, marker="s", edgecolors="#0000ff")
+
+    # find n_objects peaks 
+    kernel = np.full((4, 4), 1)
+    peaks = peak_local_max(
+        dmap, footprint=kernel, min_distance=2, num_peaks=28, exclude_border=False
+    )
+
+    pred_x = [xy[1] for xy in peaks]
+    pred_y = [xy[0] for xy in peaks]
+
+    # plot density map over og image
     axes[1].imshow(img)
 
-    green = np.array([0, 1, 0])
-    axes[1].scatter(x=marks_x, y =marks_y, c=green, s=10) #green circle, radius of 10
+    axes[1].scatter(x=pred_x, y=pred_y, c="#ff0000", s=20, marker="x") 
 
     plt.show()
 
